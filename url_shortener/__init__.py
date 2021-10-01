e5f6a78631716a0437e33ec30eec10586c2d1d6a
Zaharioaei Radu <44706212+byradu@users.noreply.github.com>
2021-10-01 22:46:02 +0300

    final version

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+'''
+We need the secret key so we can flash a error message if the provided site doesn't work.
+'''
+app.config['SECRET_KEY'] = '59881a1df4e0753c15240d165e6a3fca02f8b855aaa1307fd2069cff8844aaaa'
 
 from url_shortener import routes
--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -1,28 +1,69 @@
+from os import error
 import urllib.request
+from flask.helpers import url_for
 import pyshorteners as shortener
+from werkzeug.utils import redirect
 from url_shortener import app
 from flask import render_template,request,flash
 
+recent_history = list()
+already_computed = dict()
 
 @app.route('/',methods = ['POST','GET'])
-@app.route('/home/',methods = ['POST'])
+@app.route('/home/',methods = ['POST','GET'])
 def index():
+    '''
+    Returns to the client the blank homepage, 
+    the homepage with an error message if the link is bad or the homepage with a shorter link.
+    
+    '''
+    global recent_history,already_computed
     if request.method == 'POST':
         link = request.form['linkElement']
-        if checkUrl == False:
-            flash('Insert a running site')
-            return render_template('index.html',data='')
-        shortUrl = makeItShort(link)
-        return render_template('index.html',data=shortUrl)
-    return render_template('index.html',data='')
+        
+        if link in already_computed:
+            return render_template('index.html',data=already_computed[link],recent_history=recent_history)
 
-def checkUrl(link):
-    statusCode = urllib.request.urlopen(link).getcode()
-    print(statusCode)
-    if statusCode == 200:
-        return True
-    return False
+        recent_history = recent_history[::-1]
+        
+        if check_url(link) == False: 
+            return render_template('index.html',some_error = True,provided_link = link,recent_history=recent_history)
+        
+        short_url = make_it_short(link)
+        
+        already_computed[link] = short_url #just to not compute a new link everytime
+        recent_history.append((link,short_url))
+        recent_history = recent_history[::-1]
+        if len(recent_history) > 10:
+            recent_history = recent_history[:10]
+        
+        return render_template('index.html',data=short_url,recent_history=recent_history)
+    return render_template('index.html',data='',recent_history=recent_history)
 
-def makeItShort(link):
-    sh = shortener.Shortener()
-    return sh.tinyurl.short(link)
+
+def check_url(link):
+    '''
+    Checks if the client provides a valid URL.
+    '''
+    try:
+        statusCode = urllib.request.urlopen(link)
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        if statusCode.getcode() < 400:
+            return True
+        return False
+
+
+def make_it_short(link):
+    '''
+    Return a smaller version of the link using one of the APIs available.
+    More info at: https://pyshorteners.readthedocs.io/en/latest/
+    '''
+    try:
+        sh = shortener.Shortener()
+    except Exception as e:
+        print( str(e) )
+    else:
+        return sh.tinyurl.short(link)
